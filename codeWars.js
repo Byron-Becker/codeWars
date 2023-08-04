0feb3594f32d49f359cae3d0ed3b7c13bf39bf10
bbeck9123 <byron.ptgo@gmail.com>
2023-08-03 17:58:46 -0700

    testing

---
+++ codeWars.js
@@ -0,0 +1,13 @@
+// function occ (str, c) {
+//     let numOcc = 0
+
+//     for(let i =0; i < str.length; i++){
+//         if(str[i]=== c){
+//             numOcc++
+//         }
+//     }
+//     return numOcc
+// }
+
+
+// function numbOcc = (str,c) => str.match(new RegExp(c, 'ig') || []).length

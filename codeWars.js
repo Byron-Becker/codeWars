f8333135c1fcdaa133de3d546fddc66cc050330e
bbeck9123 <byron.ptgo@gmail.com>
2023-08-03 18:07:15 -0700

    codeWAR

--- codeWars.js
+++ codeWars.js
@@ -13,3 +13,38 @@
 // function numbOcc = (str,c) => str.match(new RegExp(c, 'ig') || []).length
 
 // const findNeedle = haystack => `found the needle at position` + haystack.indexOf('needle')
+
+// const minInt = arr => Math.min(...arr)
+
+// function minInt(arr){
+
+//     let min = arr[0]
+
+//     for(let i =0; i > arr.length; i++){
+//         if(arr[i] < min){
+//             min = arr[i]
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

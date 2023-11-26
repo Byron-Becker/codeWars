d38c120122ca656403244181a6f102716ce79db9
bbeck9123 <byron.ptgo@gmail.com>
2023-11-26 09:07:37 -0800

    new codewars

--- codeWars.js
+++ codeWars.js
@@ -751,7 +751,18 @@ const encryptThis = text => text
   .replace(/(^\w)(\w)(\w*)(\w$)/, `$1$4$3$2`)
   .replace(/^\w/,
 
+  ou are given a secret message you need to decipher. Here are the things you need to know to decipher it:
 
+  For each word:
+  
+      the second and the last letter is switched (e.g. Hello becomes Holle)
+      the first letter is replaced by its character code (e.g. H becomes 72)
+  
+  Note: there are no special characters used, only letters and spaces
+  
+  Examples
+  
+  decipherThis('72olle 103doo 100ya'); // 'Hello good day'
 
 
 

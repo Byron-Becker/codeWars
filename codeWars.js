9eb9fd28d8632ac681beaadfb21e7309abb843ee
bbeck9123 <byron.ptgo@gmail.com>
2023-11-28 08:06:54 -0800

    multiples of 3 and 5

--- codeWars.js
+++ codeWars.js
@@ -569,8 +569,13 @@ In its most basic form a spoonerism is a two word phrase in which only the first
 
 "not picking" --> "pot nicking"
 
-Your task is to create a function that takes a string of two words, separated by a space: words and returns a spoonerism of those words in a string, as in the above example. A "word" in the context of this kata can contain any of the letters A through Z in upper or lower case, and the numbers 0 to 9. Though spoonerisms are about letters in words in the domain of written and spoken language, numbers are included in the inputs for the random test cases and they require no special treatment.
+Your task is to create a functi
 
+
+on that takes a string of two words, separated by a space: words and returns a spoonerism of those words in a string, as in the above example. A "word" in the context of this kata can contain any of the letters A through Z in upper or lower case, and the numbers 0 to 9. Though spoonerisms are about letters in words in the domain of written and spoken language, numbers are included in the inputs for the random test cases and they require no special treatment.
+
+
+asdsdf
 
 
 

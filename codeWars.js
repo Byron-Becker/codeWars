77976b154ca2dc4c1348dc6ddeae14cfac5e2627
bbeck9123 <byron.ptgo@gmail.com>
2023-11-28 08:05:00 -0800

    multiples of 3 and 5

--- codeWars.js
+++ codeWars.js
@@ -551,6 +551,18 @@ for(let i = 1; i<=num.length)
 // for  y-remainder
 
 
+function solution(number){
+    var sum = 0;
+    
+    for(var i = 1;i< number; i++){
+      if(i % 3 == 0 || i % 5 == 0){
+        sum += i
+      }
+    }
+    return sum;
+  }
+
+
 
 
 

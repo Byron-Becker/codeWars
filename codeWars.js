4ce26eb34ab70aa9364f21a0dfd350dd12d4ecfb
bbeck9123 <byron.ptgo@gmail.com>
2023-11-29 09:07:09 -0800

    new codewars

--- codeWars.js
+++ codeWars.js
@@ -745,6 +745,13 @@ function balancedNum(number) {
   }
 };
 
+const encryptThis = text => text
+  .split(' ')
+  .map(word => word
+  .replace(/(^\w)(\w)(\w*)(\w$)/, `$1$4$3$2`)
+  .replace(/^\w/,
+
+
 
 
 

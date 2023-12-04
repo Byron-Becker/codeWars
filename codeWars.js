60c27bed713ec993224d0c3fa98325027938e8bb
bbeck9123 <byron.ptgo@gmail.com>
2023-12-04 07:55:49 -0800

    new codewars

--- codeWars.js
+++ codeWars.js
@@ -702,10 +702,27 @@ const ageBrackets = [
 
 const toNumberArray = (stringarray) => stringarray.map((e) => Number(e))
 
+// Given a mixed array of number and string representations of integers, add up the non-string integers and subtract the total of the string integers.
+
+Return as a number.
+
+
 
 unction divCon(x){
   return x.reduce((acc, cur) => typeof cur === 'number'? acc + cur : acc - Number(cur),0)
 }
+
+function divCon(x){
+  const strings = x.filter(n => typeof n === 'string')
+  const nums = x.filter(n => typeof n === 'number')
+  
+  const strSum = strings.reduce((acc, str) => acc += +str, 0)
+  const numSum = nums.reduce((acc, str) => acc += str, 0)
+
+  return numSum - strSum
+}
+
+
 
 
 

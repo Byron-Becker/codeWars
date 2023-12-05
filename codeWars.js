657c4633805ab153fb063df02efdefdc452078ae
bbeck9123 <byron.ptgo@gmail.com>
2023-12-05 06:57:53 -0800

    encyrption

--- codeWars.js
+++ codeWars.js
@@ -802,7 +802,15 @@ const encryptThis = text => text
        encrypted += crypt + ' ';
 
 
+// turn vowels into numbers
+function encode(string){
+  return string.replace(/[aeiou]/g, function (x) { return '_aeiou'.indexOf(x) });
+}
 
+//turn numbers back into vowels
+function decode(string){
+  return string.replace(/[1-5]/g, function (x) { return '_aeiou'.charAt(x) });
+}
 
 
 
